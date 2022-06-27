4716b66bfd91d32340e2d4177b68b2ceda4da29f
KhangTT <Khangtt@rikkeisoft.com>
2022-06-27 18:18:50 +0700

    search api

---
+++ src/MovieCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const MovieCard = ({ data }) => {
+    return (
+        <div className='movie'>
+            <div>
+                <p>{data.Year}</p>
+            </div>
+            <div>
+                <img src={data.Poster !== 'N/A' ? data.Poster : "https://via.placeholder.com/400"} alt={data.Title} />
+            </div>
+            <div>
+                <span>{data.Type}</span>
+                <h3>{data.Title}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
